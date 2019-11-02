f3ea0718830fd11e912a6e6ed77753ddd86d104d
Juan Carlos <jcrpelaez@yahoo.com>
2019-11-01 22:39:35 -0500

    added on click function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,3 +14,16 @@ function renderButtons(searchArray, classToAdd, areaToAddTo) {
     $(areaToAddTo).append(a);
   }
 }
+$(document).on("click", ".searchButton", function() {
+  var type = $(this).data("type");
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    type +
+    "&api_key=fvsPLyFNdk4oX3B1SoaoTzJlb4BDSMkx&limit=10";
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+    console.log(response);
+  });
+});
